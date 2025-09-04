ef3d00f24060252e8f47e8f665abd18436edf2e4
KennyDiedxD <cartmen95@gmail.com>
2025-09-04 23:21:34 +0800

    Remove conflicting api/ping.js; keep ping.cjs (and callback.cjs)

--- api/ping.js
+++
@@ -1,7 +0,0 @@
-// api/ping.js
-module.exports = (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ ok: true, at: new Date().toISOString() }));
-};
-module.exports.config = { runtime: "nodejs20.x" };
