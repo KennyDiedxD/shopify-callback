0590116c7998ad3bd45ee5d1106ecea0d49bff32
KennyDiedxD <cartmen95@gmail.com>
2025-09-04 23:12:48 +0800

    Add /api/echo-state and route to .cjs handlers

--- api/echo-state.cjs
+++ api/echo-state.cjs
@@ -3,11 +3,7 @@ module.exports = (req, res) => {
   const url = new URL(req.url, "https://dummy.host");
   const state = url.searchParams.get("state") || "";
   const expected = process.env.EXPECTED_STATE || "";
-
-  const toHex = s => [...Buffer.from(s, "utf8")]
-    .map(b => b.toString(16).padStart(2, "0"))
-    .join(" ");
-
+  const toHex = s => [...Buffer.from(s, "utf8")].map(b => b.toString(16).padStart(2,"0")).join(" ");
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   res.end(JSON.stringify({
     state,
@@ -17,3 +13,4 @@ module.exports = (req, res) => {
     hex: { state: toHex(state), expected: toHex(expected) }
   }));
 };
+
